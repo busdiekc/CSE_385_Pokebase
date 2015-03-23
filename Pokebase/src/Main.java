0beb6ec03744a584f90e49012496280dc4fbc5cc
jenneydc <jenneydc@miamioh.edu>
2015-03-23 13:26:33 -0400

    Added some megas, Gen 5 starters

--- Pokebase/src/Main.java
+++ Pokebase/src/Main.java
@@ -15,8 +15,20 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 	public static void main(String[] args) throws Exception	{
-        //ManipulateSprites ms = new ManipulateSprites();
-	//ms.insertAllSprites();
+        ManipulateSprites ms = new ManipulateSprites();
+	  //ms.insertAllSprites();
+       /* ms.insertSprites(3, "mega Venusaur");
+        ms.insertSprites(6, "mega Charizard Y");
+        ms.insertSprites(6, "mega Charizard X");
+        ms.insertSprites(9, "mega Blastoise");
+        ms.insertSprites(65, "mega Alakazam");
+        ms.insertSprites(94, "mega Gengar");
+        ms.insertSprites(115, "mega Kangaskhan");
+        ms.insertSprites(127, "mega Pinsir");
+        ms.insertSprites(130, "mega Gyarados");
+        ms.insertSprites(142, "mega Aerodactyl");
+        ms.insertSprites(150, "mega Mewtwo X");
+        ms.insertSprites(150, "mega Mewtwo Y"); */
 	
         WebLookAndFeel.install();
      
